dbd07ef2374511749262454e3efc50d403412cfb
Kao Viboth <kaoviboth25@gmail.com>
2026-09-15 20:49:50 +0700

    feat(inventory): update the size of inventory's table

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -3,9 +3,9 @@ import SelectItems from "./SelectItems";
 function FilterBar() {
   return (
     <div className="flex md:gap-5 lg:gap-8">
-      <SelectItems placeholder={"filter by category"} />
-      <SelectItems placeholder={"filter by model"} />
-      <SelectItems placeholder={"sortby"} width={150} />
+      <SelectItems placeholder={"Filter by category"} />
+      <SelectItems placeholder={"Filter by model"} />
+      <SelectItems placeholder={"Sortby"} width={150} />
     </div>
   );
 }
--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -19,7 +19,7 @@ function TableRow({ item }) {
   if (isDeleting) return <Spinner />;
 
   return (
-    <div className="grid w-full grid-cols-[repeat(2,5rem)_repeat(2,1fr)_10rem_10rem_10rem_5rem] grid-rows-1 gap-8 border-b border-gray-300 py-4 pl-12 text-[1.4rem] md:grid-cols-[repeat(2,5rem)_repeat(2,1fr)_repeat(3,4rem)_5rem] md:pl-6 md:text-[1.4rem] lg:grid-cols-[repeat(2,5rem)_repeat(2,1fr)_repeat(3,10rem)_5rem]">
+    <div className="grid w-full grid-cols-[repeat(2,5rem)_repeat(2,1fr)_10rem_10rem_10rem_5rem] grid-rows-1 gap-8 border-b border-gray-300 py-4 pl-12 text-[1.4rem] md:grid-cols-[repeat(2,5rem)_repeat(2,1fr)_repeat(3,4rem)_5rem] md:pl-6 md:text-[1.4rem] lg:grid-cols-[repeat(2,5rem)_repeat(5,1fr)_5rem]">
       <p className="text-center">{productId}</p>
       <img src="./assets/images/1.jpg" />
       <p>{name}</p>
